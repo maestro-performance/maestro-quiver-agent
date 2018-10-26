82a6962e1644b101331d39631951bb6ece8abeee
Otavio Rodolfo Piske <opiske@redhat.com>
2018-10-26 15:23:09 +0200

    FIX: do no publish a Ok response for every ping command

--- requests/ping/PingHandler.groovy
+++ requests/ping/PingHandler.groovy
@@ -1,8 +1,6 @@
 package org.maestro.agent.ext.requests.ping
 
 import org.maestro.agent.base.AbstractHandler
-import org.maestro.client.exchange.MaestroTopics
-import org.maestro.client.notes.OkResponse
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,9 +10,6 @@ class PingHandler extends AbstractHandler {
 
     @Override
     Object handle() {
-        logger.info("Hello from a sample Maestro Extension point");
-
-        this.getClient().publish(MaestroTopics.MAESTRO_TOPIC, new OkResponse())
         return null
     }
 }
