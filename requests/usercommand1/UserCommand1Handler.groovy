e7f6e2ce3bca5244b70e958a4049b0db318e2991
Otavio Rodolfo Piske <opiske@redhat.com>
2018-11-01 15:00:42 +0100

    UPD: simplifies the code to reuse methods from the common code base

--- requests/usercommand1/UserCommand1Handler.groovy
+++ requests/usercommand1/UserCommand1Handler.groovy
@@ -3,37 +3,18 @@ package org.maestro.agent.ext.requests.genericrequest
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.maestro.client.notes.*;
-import org.maestro.common.worker.TestLogUtils;
 
 import org.maestro.agent.base.AbstractHandler
 
-import java.io.File
 
 class UserCommand1Handler extends AbstractHandler {
     private static final Logger logger = LoggerFactory.getLogger(UserCommand1Handler.class);
 
     @Override
     Object handle() {
-        String baseLogDirStr = System.getProperty("maestro.log.dir")
-
-        if (baseLogDirStr == null) {
-            logger.error("Cannot continue without the log directory");
-
-            this.getClient().notifyFailure(getCurrentTest(), "The log directory is not set on the agent")
-
-            return null
-        }
-
-        File baseLogDir = new File(baseLogDirStr);
-        File testLogDir = TestLogUtils.nextTestLogDir(baseLogDir);
-        String logDir = testLogDir.getPath();
-
-        logger.info("Erasing old data")
-        "rm -rf ${logDir}/quiver".execute();
+        String logDir = getTestLogDir().getPath()
 
         logger.info("Running Quiver")
-        def workerOptions = getWorkerOptions();
-
         String command = "quiver --output ${logDir}"
 
         UserCommand1Request request = (UserCommand1Request) getNote()
@@ -45,7 +26,7 @@ class UserCommand1Handler extends AbstractHandler {
         }
 
         try {
-            command = command + " " + workerOptions.getBrokerURL()
+            command = command + " " + getWorkerOptions().getBrokerURL()
             if (super.executeOnShell(command) != 0) {
                 logger.warn("Unable to execute the Quiver test")
                 this.getClient().notifyFailure(getCurrentTest(), "Unable to execute the Quiver test")
@@ -53,14 +34,14 @@ class UserCommand1Handler extends AbstractHandler {
                 return null
             }
 
-            TestLogUtils.createSymlinks(baseLogDir, false);
+            createTestSuccessSymlinks();
 
             this.getClient().notifySuccess(getCurrentTest(), "Quiver test ran successfully")
             logger.info("Quiver test ran successfully")
 
         }
         catch (Exception e) {
-            TestLogUtils.createSymlinks(baseLogDir, true);
+            createTestFailSymlinks();
 
             this.getClient().notifyFailure(getCurrentTest(), e.getMessage())
 
